afadb749e2aadad4ebd0bd5259c171acf510162a
koturn <jeak.koutan.apple@gmail.com>
2022-10-30 11:56:19 +0900

    Use runtime conditional branch in non multi shaders

--- Assets/koturn/LilEmissionWave/Shaders/custom_insert.hlsl
+++ Assets/koturn/LilEmissionWave/Shaders/custom_insert.hlsl
@@ -2,12 +2,22 @@ float3 getEmissionPos(float3 positionOS)
 {
 #if defined(_WAVEPOSSPACE_OBJECT)
     return positionOS;
-#else
+#elif defined(_WAVEPOSSPACE_OBJECT)
     return mul((float3x3)unity_ObjectToWorld, positionOS)
         / float3(
             length(unity_ObjectToWorld._m00_m10_m20),
             length(unity_ObjectToWorld._m01_m11_m21),
             length(unity_ObjectToWorld._m02_m12_m22));
+#else
+    if (_WavePosSpace == 0) {
+        return positionOS;
+    } else {
+        return mul((float3x3)unity_ObjectToWorld, positionOS)
+            / float3(
+                length(unity_ObjectToWorld._m00_m10_m20),
+                length(unity_ObjectToWorld._m01_m11_m21),
+                length(unity_ObjectToWorld._m02_m12_m22));
+    }
 #endif
 }
 
@@ -20,12 +30,25 @@ float pickupPosition(float3 pos)
     return pos.y;
 #elif defined(_WAVEAXIS_Z)
     return pos.z;
-#else
+#elif defined(_WAVEAXIS_FREE)
     float3 s3, c3;
     sincos(_WaveAxisAngles, s3, c3);
     pos.yz = mul(float2x2(c3.x, -s3.x, s3.x, c3.x), pos.yz);
     pos.zx = mul(float2x2(c3.y, -s3.y, s3.y, c3.y), pos.zx);
     pos.xy = mul(float2x2(c3.z, -s3.z, s3.z, c3.z), pos.xy);
     return pos.y;
+#else
+    if (_WaveAxis == 3) {
+        float3 s3, c3;
+        sincos(_WaveAxisAngles, s3, c3);
+        pos.yz = mul(float2x2(c3.x, -s3.x, s3.x, c3.x), pos.yz);
+        pos.zx = mul(float2x2(c3.y, -s3.y, s3.y, c3.y), pos.zx);
+        pos.xy = mul(float2x2(c3.z, -s3.z, s3.z, c3.z), pos.xy);
+        return pos.y;
+    } else {
+        return _WaveAxis == 0 ? pos.x
+            : _WaveAxis == 1 ? pos.y
+            : pos.z;
+    }
 #endif
 }
