294d30dab0394096ea9222f5085ad8f193e0984d
koturn <jeak.koutan.apple@gmail.com>
2023-01-14 19:58:13 +0900

    Add constants for enum variable

--- Assets/koturn/LilEmissionWave/Shaders/custom_insert.hlsl
+++ Assets/koturn/LilEmissionWave/Shaders/custom_insert.hlsl
@@ -1,24 +1,38 @@
 #ifdef LIL_MULTI
 #    if defined(_WAVEPOSSPACE_OBJECT)
-#        define _WavePosSpace 0
+#        define _WavePosSpace kWavePosSpaceObject
 #    elif defined(_WAVEPOSSPACE_WORLD)
-#        define _WavePosSpace 1
+#        define _WavePosSpace kWavePosSpaceWorld
 #    endif  // defined(_WAVEPOSSPACE_OBJECT)
 #    if defined(_WAVEAXIS_X)
-#        define _WaveAxis 0
+#        define _WaveAxis kWaveAxisX
 #    elif defined(_WAVEAXIS_Y)
-#        define _WaveAxis 1
+#        define _WaveAxis kWaveAxisY
 #    elif defined(_WAVEAXIS_Z)
-#        define _WaveAxis 2
+#        define _WaveAxis kWaveAxisZ
 #    elif defined(_WAVEAXIS_FREE)
-#        define _WaveAxis 3
+#        define _WaveAxis kWaveAxisFree
 #    endif
 #endif  // LIL_MULTI
 
 
+//! Enum value for _WavePosSpace, "Object".
+static const int kWavePosSpaceObject = 0;
+//! Enum value for _WavePosSpace, "World".
+static const int kWavePosSpaceWorld = 1;
+//! Enum value for _WaveAxis, "X".
+static const int kWaveAxisX = 0;
+//! Enum value for _WaveAxis, "Y".
+static const int kWaveAxisY = 1;
+//! Enum value for _WaveAxis, "Z".
+static const int kWaveAxisZ = 2;
+//! Enum value for _WaveAxis, "Free".
+static const int kWaveAxisFree = 3;
+
+
 float3 getEmissionPos(float3 positionOS)
 {
-    if (_WavePosSpace == 0) {
+    if (_WavePosSpace == kWavePosSpaceObject) {
         return positionOS;
     } else {
         return mul((float3x3)unity_ObjectToWorld, positionOS)
@@ -32,7 +46,7 @@ float3 getEmissionPos(float3 positionOS)
 
 float pickupPosition(float3 pos)
 {
-    if (_WaveAxis == 3) {
+    if (_WaveAxis == kWaveAxisFree) {
         float3 s3, c3;
         sincos(_WaveAxisAngles, s3, c3);
         pos.yz = mul(float2x2(c3.x, -s3.x, s3.x, c3.x), pos.yz);
@@ -40,8 +54,8 @@ float pickupPosition(float3 pos)
         pos.xy = mul(float2x2(c3.z, -s3.z, s3.z, c3.z), pos.xy);
         return pos.y;
     } else {
-        return _WaveAxis == 0 ? pos.x
-            : _WaveAxis == 1 ? pos.y
+        return _WaveAxis == kWaveAxisX ? pos.x
+            : _WaveAxis == kWaveAxisY ? pos.y
             : pos.z;
     }
 }
