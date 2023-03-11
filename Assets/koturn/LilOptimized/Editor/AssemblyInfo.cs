9755cfff535903509e2fcfbd3e61ef60195763dc
koturn <jeak.koutan.apple@gmail.com>
2023-03-11 16:03:21 +0900

    Add an editor extension for koturn/LilOptimized

---
+++ Assets/koturn/LilOptimized/Editor/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompany("koturn")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright (C) 2022 koturn All Rights Reserverd.")]
+[assembly: AssemblyDescription("Material inspector of koturn/LilOptimized.")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyProduct("Koturn.LilOptimized")]
+[assembly: AssemblyTitle("Koturn.LilOptimized")]
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/LillKustomShaders")]
