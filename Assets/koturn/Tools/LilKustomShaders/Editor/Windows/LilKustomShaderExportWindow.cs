1ff11bdf73f7df95b53ff4e18250823b1f56d660
koturn <jeak.koutan.apple@gmail.com>
2023-08-19 11:00:46 +0900

    Enable to export zip package for VPM

--- Assets/koturn/Tools/LilKustomShaders/Editor/Windows/LilKustomShaderExportWindow.cs
+++ Assets/koturn/Tools/LilKustomShaders/Editor/Windows/LilKustomShaderExportWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.IO.Compression;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,6 +12,11 @@ namespace Koturn.Tools.LilKustomShaders.Windows
     /// </summary>
     public class ExportWindow : EditorWindow
     {
+        /// <summary>
+        /// Version string.
+        /// </summary>
+        private const string Version = "1.1.0";
+
         /// <summary>
         /// Entire json data.
         /// </summary>
@@ -29,13 +35,21 @@ namespace Koturn.Tools.LilKustomShaders.Windows
         class PackageInfo
         {
             /// <summary>
-            /// Package name for export.
+            /// Unity package name for export.
             /// </summary>
-            public string Name;
+            public string UnityPackageName;
+            /// <summary>
+            /// VPM name.
+            /// </summary>
+            public string VpmName;
             /// <summary>
             /// Asset path array.
             /// </summary>
-            public string[] AssetPaths;
+            public string AssetPath;
+            /// <summary>
+            /// Asset path array.
+            /// </summary>
+            public string[] DependAssetPaths;
         }
 
 
@@ -78,13 +92,15 @@ namespace Koturn.Tools.LilKustomShaders.Windows
         /// </summary>
         private void ExportPackages()
         {
-            var directoryPath = Directory.Exists(_lastExportDirectoryPath) ? _lastExportDirectoryPath : Application.dataPath;
-            var path = EditorUtility.SaveFolderPanel("Select export directory", directoryPath, string.Empty);
-            if (string.IsNullOrEmpty(path))
+            var exportDirPath = EditorUtility.SaveFolderPanel(
+                "Select export directory",
+                Directory.Exists(_lastExportDirectoryPath) ? _lastExportDirectoryPath : Application.dataPath,
+                string.Empty);
+            if (string.IsNullOrEmpty(exportDirPath))
             {
                 return;
             }
-            _lastExportDirectoryPath = path;
+            _lastExportDirectoryPath = exportDirPath;
 
             var jsonPath = Path.Combine(Application.dataPath, "koturn/Tools/LilKustomShaders/Editor/Windows/ExportConfig.json");
             if (!File.Exists(jsonPath))
@@ -100,19 +116,96 @@ namespace Koturn.Tools.LilKustomShaders.Windows
 
             foreach (var package in jsonData.Packages)
             {
-                var packageName = package.Name;
-                if (!packageName.EndsWith(".unitypackage"))
-                {
-                    packageName += ".unitypackage";
-                }
+                ExportAsUnityPackage(
+                    Path.Combine(exportDirPath, package.UnityPackageName),
+                    package.AssetPath,
+                    package.DependAssetPaths);
+                ExportAsZipArchive(
+                    Path.Combine(exportDirPath, $"{package.VpmName}-{Version}.zip"),
+                    package.AssetPath);
+            }
+        }
+
+        /// <summary>
+        /// Export asset and dependent assets as unitypackage file.
+        /// </summary>
+        /// <param name="unityPackagePath">Unitypackage file path for export.</param>
+        /// <param name="assetPath">Target asset path.</param>
+        /// <param name="dependAssetPaths">Dependent asset paths.</param>
+        private void ExportAsUnityPackage(string unityPackagePath, string assetPath, string[] dependAssetPaths)
+        {
+            if (!unityPackagePath.EndsWith(".unitypackage"))
+            {
+                unityPackagePath += ".unitypackage";
+            }
+
+            if (dependAssetPaths is null || dependAssetPaths.Length == 0)
+            {
+                AssetDatabase.ExportPackage(
+                    assetPath,
+                    unityPackagePath,
+                    ExportPackageOptions.Recurse);
+            }
+            else
+            {
+                var assetPaths = new string[dependAssetPaths.Length + 1];
+                assetPaths[0] = assetPath;
+                Array.Copy(dependAssetPaths, 0, assetPaths, 1, dependAssetPaths.Length);
 
                 AssetDatabase.ExportPackage(
-                    package.AssetPaths,
-                    Path.Combine(path, package.Name),
+                    assetPaths,
+                    unityPackagePath,
                     ExportPackageOptions.Recurse);
-
-                Debug.Log($"Export {package.Name}");
             }
+
+            Debug.Log($"Exported {unityPackagePath}");
+        }
+
+        /// <summary>
+        /// Export asset as zip archive for VPM.
+        /// </summary>
+        /// <param name="zipFilePath">Zip file path for export.</param>
+        /// <param name="assetPath">Target asset path.</param>
+        private void ExportAsZipArchive(string zipFilePath, string assetPath)
+        {
+            File.Delete(zipFilePath);
+
+            if (!assetPath.EndsWith("/"))
+            {
+                assetPath += "/";
+            }
+
+            using (var zipArchive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create))
+            {
+                var absAssetPath = AssetPathToAbsPath(assetPath);
+                foreach (var filePath in Directory.EnumerateFiles(absAssetPath, "*", SearchOption.AllDirectories))
+                {
+                    var data = File.ReadAllBytes(filePath);
+                    var entry = zipArchive.CreateEntry(
+                        filePath.Substring(absAssetPath.Length).Replace("\\", "/"),
+                        System.IO.Compression.CompressionLevel.Optimal);
+                    using (var zs = entry.Open())
+                    {
+                        zs.Write(data, 0, data.Length);
+                    }
+                }
+            }
+
+            Debug.Log($"Exported {zipFilePath}");
+        }
+
+        /// <summary>
+        /// Convert from Assets path to Absolute path.
+        /// </summary>
+        /// <param name="assetPath">Target asset path.</param>
+        /// <returns>Absolute path converte from <paramref name="assetPath"/>.</returns>
+        private static string AssetPathToAbsPath(string assetPath)
+        {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+            return assetPath.Replace("Assets", Application.dataPath).Replace("/", "\\");
+#else
+            return assetPath.Replace("Assets", Application.dataPath);
+#endif
         }
     }
 }
