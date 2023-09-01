c5284a3c0c7044433ae1e36a5357e675da5f41d6
koturn <jeak.koutan.apple@gmail.com>
2023-09-02 06:50:21 +0900

    Fix memory leak

--- Assets/koturn/LilOptimized/Editor/Sqlite/SqliteUtil.cs
+++ Assets/koturn/LilOptimized/Editor/Sqlite/SqliteUtil.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 
 namespace Koturn.lilToon.Sqlite
@@ -26,8 +27,42 @@ namespace Koturn.lilToon.Sqlite
         /// <param name="sql">SQL to execute.</param>
         public static void Exec(SqliteHandle dbHandle, string sql)
         {
-            var result = NativeMethods.Exec(dbHandle, sql, IntPtr.Zero, IntPtr.Zero, out var errmsg);
-            SqliteException.ThrowIfFailed(result, "Failed to execute SQL: " + errmsg);
+            var result = NativeMethods.Exec(dbHandle, sql, IntPtr.Zero, IntPtr.Zero, out var pErrMsg);
+            if (result != SqliteResult.OK)
+            {
+                var errmsg = CreateFromUtf8String(pErrMsg);
+                NativeMethods.Free(pErrMsg);
+                SqliteException.Throw(result, "Failed to execute SQL: " + errmsg);
+            }
+        }
+
+
+        /// <summary>
+        /// Create <see cref="string"/> from UTF-8 byte sequence.
+        /// </summary>
+        /// <param name="p">Pointer to UTF-8 byte sequence.</param>
+        /// <returns>Created <see cref="string"/>.</returns>
+        private static string CreateFromUtf8String(IntPtr p)
+        {
+            unsafe
+            {
+                var psb = (sbyte *)p;
+                return new string(psb, 0, ByteLengthOf(psb), Encoding.UTF8);
+            }
+        }
+
+        /// <summary>
+        /// Get byte length of null-terminated string.
+        /// </summary>
+        /// <param name="psb">Pointer to null-terminated string.</param>
+        /// <returns>Byte length of null-terminated string.</returns>
+        private static unsafe int ByteLengthOf(sbyte *psb)
+        {
+            var psbEnd = psb;
+            for (; *psbEnd != 0; psbEnd++)
+            {
+            }
+            return (int)(psbEnd - psb);
         }
 
         /// <summary>
@@ -86,13 +121,23 @@ namespace Koturn.lilToon.Sqlite
             /// <param name="sql">SQL to be evaluated.</param>
             /// <param name="callback">Callback function.</param>
             /// <param name="callbackArg">1st argument to callback.</param>
-            /// <param name="errmsg">Error msg written here.</param>
+            /// <param name="pErrMsg">Error msg written here.</param>
             /// <returns>Result code.</returns>
             /// <remarks>
             /// <seealso href="https://www.sqlite.org/c3ref/exec.html"/>
             /// </remarks>
             [DllImport(LibraryName, EntryPoint = "sqlite3_exec", CallingConvention = CallConv)]
-            public static extern SqliteResult Exec(SqliteHandle dbHandle, string sql, IntPtr callback, IntPtr callbackArg, out string errmsg);
+            public static extern SqliteResult Exec(SqliteHandle dbHandle, string sql, IntPtr callback, IntPtr callbackArg, out IntPtr pErrMsg);
+
+            /// <summary>
+            /// Free memory allocated in SQLite3 functions.
+            /// </summary>
+            /// <param name="pMemory">Allocated memory pointer.</param>
+            /// <remarks>
+            /// <seealso href="https://www.sqlite.org/c3ref/free.html"/>
+            /// </remarks>
+            [DllImport(LibraryName, EntryPoint = "sqlite3_free", CallingConvention = CallConv)]
+            public static extern void Free(IntPtr pMemory);
         }
     }
 }
