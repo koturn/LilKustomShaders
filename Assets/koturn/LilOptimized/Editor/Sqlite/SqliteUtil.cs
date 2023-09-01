805c05670dcafdef5a6b71b25d72623ed7b34f78
koturn <jeak.koutan.apple@gmail.com>
2023-09-02 07:40:21 +0900

    Implement callback delegates for sqlite3_exec()

--- Assets/koturn/LilOptimized/Editor/Sqlite/SqliteUtil.cs
+++ Assets/koturn/LilOptimized/Editor/Sqlite/SqliteUtil.cs
@@ -11,6 +11,52 @@ namespace Koturn.lilToon.Sqlite
     public static class SqliteUtil
     {
         /// <summary>
+        /// Callback delegate of third argument of <see cref="SqliteUtil.Execute(SqliteHandle, string, ExecCallbackFunc, IntPtr)"/>.
+        /// </summary>
+        /// <param name="arg">Fouth argument of <see cref="SqliteUtil.Execute(SqliteHandle, string, ExecCallbackFunc, IntPtr)"/>.</param>
+        /// <param name="colCount">Number of columns.</param>
+        /// <param name="pColumns">Pointer to null-terminated string array of column text.</param>
+        /// <param name="pColumnNames">Pointer to null-terminated string array of column name.</param>
+        /// <returns>0 to continue, otherwise to abotd.</returns>
+        public delegate int ExecCallbackFunc(IntPtr arg, int colCount, IntPtr pColumns, IntPtr pColumnNames);
+        /// <summary>
+        /// Callback delegate of third argument of <see cref="SqliteLibrary.Execute(SqliteHandle, string, ExecStringCallbackFunc, object, out string)"/>.
+        /// </summary>
+        /// <param name="arg">Fouth argument of <see cref="SqliteLibrary.Execute(SqliteHandle, string, ExecStringCallbackFunc, object, out string)"/>.</param>
+        /// <param name="columns">Column text array.</param>
+        /// <param name="columnNames">Column name array.</param>
+        /// <returns>0 to continue, otherwise to abotd.</returns>
+        public delegate int ExecStringCallbackFunc(IntPtr arg, string[] columns, string[] columnNames);
+        /// <summary>
+        /// Callback delegate of third argument of <see cref="SqliteLibrary.Execute(SqliteHandle, string, ExecStringNoArgCallbackFunc, out string)"/>.
+        /// </summary>
+        /// <param name="columns">Column text array.</param>
+        /// <param name="columnNames">Column name array.</param>
+        /// <returns>0 to continue, otherwise to abotd.</returns>
+        public delegate int ExecStringNoArgCallbackFunc(string[] columns, string[] columnNames);
+        /// <summary>
+        /// Callback delegate of third argument of <see cref="SqliteUtil.Execute(SqliteHandle, string, ExecCallbackFunc, IntPtr)"/>.
+        /// </summary>
+        /// <param name="arg">Fouth argument of <see cref="SqliteUtil.Execute(SqliteHandle, string, ExecCallbackFunc, IntPtr)"/>.</param>
+        /// <param name="colCount">Number of columns.</param>
+        /// <param name="pColumns">Pointer to null-terminated string array of column text.</param>
+        /// <param name="pColumnNames">Pointer to null-terminated string array of column name.</param>
+        public delegate void ExecCallbackAction(IntPtr arg, int colCount, IntPtr pColumns, IntPtr pColumnNames);
+        /// <summary>
+        /// Callback delegate of third argument of <see cref="SqliteLibrary.Execute(SqliteHandle, string, ExecStringCallbackFunc, object, out string)"/>.
+        /// </summary>
+        /// <param name="arg">Fouth argument of <see cref="SqliteLibrary.Execute(SqliteHandle, string, ExecStringCallbackFunc, object, out string)"/>.</param>
+        /// <param name="columns">Column text array.</param>
+        /// <param name="columnNames">Column name array.</param>
+        public delegate void ExecStringCallbackAction(IntPtr arg, string[] columns, string[] columnNames);
+        /// <summary>
+        /// Callback delegate of third argument of <see cref="SqliteLibrary.Execute(SqliteHandle, string, ExecStringNoArgCallbackFunc, out string)"/>.
+        /// </summary>
+        /// <param name="columns">Column text array.</param>
+        /// <param name="columnNames">Column name array.</param>
+        public delegate void ExecStringNoArgCallbackAction(string[] columns, string[] columnNames);
+
+        /// <summary>
         /// Delegate for <see cref="NativeMethods.Open"/> or <see cref="NativeMethods.OpenW"/>.
         /// </summary>
         /// <param name="filePath">SQLite3 database file path.</param>
@@ -32,7 +78,7 @@ namespace Koturn.lilToon.Sqlite
         /// <param name="callback">Callback function.</param>
         /// <param name="callbackArg">1st argument to callback.</param>
         /// <param name="errmsgHandle">Error msg written here.</param>
-        private delegate SqliteResult ExecuteFunc(SqliteHandle db, string sql, IntPtr callback, IntPtr callbackArg, out SqliteMemoryHandle errmsgHandle);
+        private delegate SqliteResult ExecuteFunc(SqliteHandle db, string sql, ExecCallbackFunc callback, IntPtr callbackArg, out SqliteMemoryHandle errmsgHandle);
         /// <summary>
         /// Delegate for <see cref="NativeMethods.Free"/> or <see cref="NativeMethods.FreeW"/>.
         /// </summary>
@@ -120,7 +166,112 @@ namespace Koturn.lilToon.Sqlite
         /// <param name="sql">SQL to be evaluated.</param>
         public static void Execute(SqliteHandle db, string sql)
         {
-            Execute(db, sql, IntPtr.Zero, IntPtr.Zero);
+            Execute(db, sql, (ExecCallbackFunc)null, IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// Execute SQL.
+        /// </summary>
+        /// <param name="db">An open database.</param>
+        /// <param name="sql">SQL to execute.</param>
+        /// <param name="callback">Callback function.</param>
+        public static void Execute(SqliteHandle db, string sql, ExecStringNoArgCallbackAction callback)
+        {
+            Execute(
+                db,
+                sql,
+                callback is null ? null : new ExecStringNoArgCallbackFunc((columns, columnNames) =>
+                {
+                    callback(columns, columnNames);
+                    return 0;
+                }));
+        }
+
+        /// <summary>
+        /// Execute SQL.
+        /// </summary>
+        /// <param name="db">An open database.</param>
+        /// <param name="sql">SQL to execute.</param>
+        /// <param name="callback">Callback function.</param>
+        public static void Execute(SqliteHandle db, string sql, ExecStringNoArgCallbackFunc callback)
+        {
+            Execute(
+                db,
+                sql,
+                callback is null ? null : new ExecCallbackFunc((arg, colCount, pColumns, pColumnNames) =>
+                {
+                    var columns = new string[colCount];
+                    var columnNames = new string[colCount];
+
+                    unsafe
+                    {
+                        var psbColumns = (sbyte **)pColumns;
+                        var psbColumnNames = (sbyte **)pColumnNames;
+
+                        for (int i = 0; i < columns.Length; i++)
+                        {
+                            columns[i] = CreateFromUtf8String(psbColumns[i]);
+                            columnNames[i] = CreateFromUtf8String(psbColumnNames[i]);
+                        }
+                    }
+
+                    return callback(columns, columnNames);
+                }),
+                IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// Execute SQL.
+        /// </summary>
+        /// <param name="db">An open database.</param>
+        /// <param name="sql">SQL to execute.</param>
+        /// <param name="callback">Callback function.</param>
+        /// <param name="callbackArg">1st argument to callback.</param>
+        public static void Execute(SqliteHandle db, string sql, ExecStringCallbackAction callback, IntPtr callbackArg)
+        {
+            Execute(
+                db,
+                sql,
+                callback is null ? null : new ExecStringCallbackFunc((arg, columns, columnNames) =>
+                {
+                    callback(arg, columns, columnNames);
+                    return 0;
+                }),
+                callbackArg);
+        }
+
+        /// <summary>
+        /// Execute SQL.
+        /// </summary>
+        /// <param name="db">An open database.</param>
+        /// <param name="sql">SQL to execute.</param>
+        /// <param name="callback">Callback function.</param>
+        /// <param name="callbackArg">1st argument to callback.</param>
+        public static void Execute(SqliteHandle db, string sql, ExecStringCallbackFunc callback, IntPtr callbackArg)
+        {
+            Execute(
+                db,
+                sql,
+                callback is null ? null : new ExecCallbackFunc((arg, colCount, pColumns, pColumnNames) =>
+                {
+                    var columns = new string[colCount];
+                    var columnNames = new string[colCount];
+
+                    unsafe
+                    {
+                        var psbColumns = (sbyte **)pColumns;
+                        var psbColumnNames = (sbyte **)pColumnNames;
+
+                        for (int i = 0; i < columns.Length; i++)
+                        {
+                            columns[i] = CreateFromUtf8String(psbColumns[i]);
+                            columnNames[i] = CreateFromUtf8String(psbColumnNames[i]);
+                        }
+                    }
+
+                    return callback(arg, columns, columnNames);
+                }),
+                callbackArg);
         }
 
         /// <summary>
@@ -130,7 +281,7 @@ namespace Koturn.lilToon.Sqlite
         /// <param name="sql">SQL to be evaluated.</param>
         /// <param name="callback">Callback function.</param>
         /// <param name="callbackArg">1st argument to callback.</param>
-        public static void Execute(SqliteHandle db, string sql, IntPtr callback, IntPtr callbackArg)
+        public static void Execute(SqliteHandle db, string sql, ExecCallbackFunc callback, IntPtr callbackArg)
         {
             var result = _execute(db, sql, callback, callbackArg, out var errmsgHandle);
             if (result != SqliteResult.OK)
@@ -164,9 +315,18 @@ namespace Koturn.lilToon.Sqlite
         {
             unsafe
             {
-                var psb = (sbyte *)p;
-                return new string(psb, 0, ByteLengthOf(psb), Encoding.UTF8);
+                return CreateFromUtf8String((sbyte *)p);
             }
+        }
+
+        /// <summary>
+        /// Create <see cref="string"/> from UTF-8 byte sequence.
+        /// </summary>
+        /// <param name="psb">Pointer to UTF-8 byte sequence.</param>
+        /// <returns>Created <see cref="string"/>.</returns>
+        private static unsafe string CreateFromUtf8String(sbyte *psb)
+        {
+            return new string(psb, 0, ByteLengthOf(psb), Encoding.UTF8);
         }
 
         /// <summary>
@@ -224,7 +384,7 @@ namespace Koturn.lilToon.Sqlite
             /// <seealso href="https://www.sqlite.org/c3ref/exec.html"/>
             /// </remarks>
             [DllImport("sqlite3", EntryPoint = "sqlite3_exec", CallingConvention = CallingConvention.Cdecl)]
-            public static extern SqliteResult Execute(SqliteHandle db, string sql, IntPtr callback, IntPtr callbackArg, out SqliteMemoryHandle errmgHandle);
+            public static extern SqliteResult Execute(SqliteHandle db, string sql, ExecCallbackFunc callback, IntPtr callbackArg, out SqliteMemoryHandle errmgHandle);
 
             /// <summary>
             /// Free memory allocated in SQLite3 functions.
@@ -273,7 +433,7 @@ namespace Koturn.lilToon.Sqlite
             /// <seealso href="https://www.sqlite.org/c3ref/exec.html"/>
             /// </remarks>
             [DllImport("winsqlite3", EntryPoint = "sqlite3_exec", CallingConvention = CallingConvention.StdCall)]
-            public static extern SqliteResult ExecuteW(SqliteHandle db, string sql, IntPtr callback, IntPtr callbackArg, out SqliteMemoryHandle errmsgHandle);
+            public static extern SqliteResult ExecuteW(SqliteHandle db, string sql, ExecCallbackFunc callback, IntPtr callbackArg, out SqliteMemoryHandle errmsgHandle);
 
             /// <summary>
             /// Free memory allocated in SQLite3 functions.
