c25a27a6884073c2d3e4f77432b6f3602cc3a33c
koturn <jeak.koutan.apple@gmail.com>
2024-05-01 17:44:28 +0900

    chore: Bump up version Number: 1.1.2 -> 1.1.3

--- Assets/koturn/LilHueShift/Editor/AssemblyInfo.cs
+++ Assets/koturn/LilHueShift/Editor/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
 [assembly: AssemblyCompany("koturn")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCopyright("Copyright (C) 2022 koturn All Rights Reserverd.")]
 [assembly: AssemblyDescription("Material inspector of koturn/LilHueShift.")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
-[assembly: AssemblyInformationalVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyInformationalVersion("1.1.3.0")]
 [assembly: AssemblyProduct("Koturn.LilHueShift")]
 [assembly: AssemblyTitle("Koturn.LilHueShift")]
 [assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/LillKustomShaders")]
--- Assets/koturn/Tools/LilKustomShaders/Editor/AssemblyInfo.cs
+++ Assets/koturn/Tools/LilKustomShaders/Editor/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
 [assembly: AssemblyCompany("koturn")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCopyright("Copyright (C) 2023 koturn All Rights Reserverd.")]
 [assembly: AssemblyDescription("Utilities for koturn/LilKustomShaders.")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
-[assembly: AssemblyInformationalVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyInformationalVersion("1.1.3.0")]
 [assembly: AssemblyProduct("Koturn.Tools.LilKustomShaders")]
 [assembly: AssemblyTitle("Koturn.Tools.LilKustomShaders")]
 [assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/LillKustomShaders")]
