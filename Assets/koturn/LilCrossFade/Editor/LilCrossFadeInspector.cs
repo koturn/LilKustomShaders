908b5dde41bb8f1e4c6c99ca575e58d1fa180faf
koturn <jeak.koutan.apple@gmail.com>
2024-12-15 10:52:09 +0900

    feat: Implement atlas texture mode

--- Assets/koturn/LilCrossFade/Editor/LilCrossFadeInspector.cs
+++ Assets/koturn/LilCrossFade/Editor/LilCrossFadeInspector.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using lilToon;
@@ -21,10 +21,6 @@ namespace Koturn.lilToon
         private static bool isShowCustomProperties;
 
         /// <summary>
-        /// <see cref="MaterialProperty"/> of "_MainTexArray".
-        /// </summary>
-        private MaterialProperty _mainTexArray;
-        /// <summary>
         /// <see cref="MaterialProperty"/> of "_DisplayTime".
         /// </summary>
         private MaterialProperty _displayCycleTime;
@@ -33,9 +29,50 @@ namespace Koturn.lilToon
         /// </summary>
         private MaterialProperty _crossFadeTime;
         /// <summary>
+        /// <see cref="MaterialProperty"/> of "_TexMode".
+        /// </summary>
+        private MaterialProperty _texMode;
+        /// <summary>
         /// <see cref="MaterialProperty"/> of "_NumTextures".
         /// </summary>
         private MaterialProperty _numTextures;
+        /// <summary>
+        /// <see cref="MaterialProperty"/> of "_MainTexArray".
+        /// </summary>
+        private MaterialProperty _mainTexArray;
+        /// <summary>
+        /// <see cref="MaterialProperty"/> of "_MainTex", "_MainTex2", "_MainTex3" and "_MainTex4".
+        /// </summary>
+        private MaterialProperty[] _mainTexes;
+        /// <summary>
+        /// <see cref="MaterialProperty"/> of "_AtlasRows".
+        /// </summary>
+        private MaterialProperty _atlasRows;
+        /// <summary>
+        /// <see cref="MaterialProperty"/> of "_AtlasCols".
+        /// </summary>
+        private MaterialProperty _atlasCols;
+        /// <summary>
+        /// Keywords to preserve.
+        /// </summary>
+        private List<string> _shaderKeywords = new List<string>();
+
+        /// <summary>
+        /// Draw property items.
+        /// </summary>
+        /// <param name="me">The <see cref="MaterialEditor"/> that are calling this <see cref="OnGUI(MaterialEditor, MaterialProperty[])"/> (the 'owner').</param>
+        /// <param name="mps">Material properties of the current selected shader.</param>
+        public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] props)
+        {
+            base.OnGUI(materialEditor, props);
+
+            var material = (Material)materialEditor.target;
+            foreach (var keyword in _shaderKeywords)
+            {
+                material.EnableKeyword(keyword);
+            }
+            _shaderKeywords.Clear();
+        }
 
         /// <summary>
         /// Load custom language file and make cache of shader properties.
@@ -52,10 +89,20 @@ namespace Koturn.lilToon
 
             LoadCustomLanguage(AssetGuid.LangCustom);
 
-            _mainTexArray = FindProperty("_MainTexArray", props);
             _displayCycleTime = FindProperty("_DisplayTime", props);
             _crossFadeTime = FindProperty("_CrossFadeTime", props);
+            _texMode = FindProperty("_TexMode", props);
             _numTextures = FindProperty("_NumTextures", props);
+            _mainTexArray = FindProperty("_MainTexArray", props);
+            _mainTexes = new[]
+            {
+                FindProperty("_MainTex", props),
+                FindProperty("_MainTex2", props),
+                FindProperty("_MainTex3", props),
+                FindProperty("_MainTex4", props)
+            };
+            _atlasRows = FindProperty("_AtlasRows", props);
+            _atlasCols = FindProperty("_AtlasCols", props);
         }
 
         /// <summary>
@@ -85,25 +132,90 @@ namespace Koturn.lilToon
                 using (new EditorGUILayout.VerticalScope(boxInnerHalf))
                 {
                     var me = m_MaterialEditor;
-                    me.ShaderProperty(_mainTexArray, GetLoc("sMainTexArray"));
+                    me.ShaderProperty(_displayCycleTime, GetLoc("sDisplayTime"));
+                    me.ShaderProperty(_crossFadeTime, GetLoc("sCrossFadeTime"));
+                    me.ShaderProperty(_texMode, GetLoc("sTexMode"));
 
-                    var texValue = _mainTexArray.textureValue;
-                    var depth = texValue is null ? 0.0f : (float)((Texture2DArray)texValue).depth;
-                    using (new EditorGUI.DisabledScope(texValue is null))
+                    var texMode = (TexMode)_texMode.floatValue;
+                    if (texMode == TexMode.Textures)
                     {
+                        var texCount = 1;
+                        for (int i = _mainTexes.Length - 1; i >= 0; i--)
+                        {
+                            if (_mainTexes[i].textureValue != null)
+                            {
+                                texCount = i + 1;
+                                break;
+                            }
+                        }
+
                         me.ShaderProperty(_numTextures, GetLoc("sNumTextures"));
                         if (GUILayout.Button("Set to texture count"))
                         {
-                            _numTextures.floatValue = depth;
+                            _numTextures.floatValue = texCount;
                         }
                         else
                         {
-                            _numTextures.floatValue = Math.Max(1.0f, Math.Min(depth, (float)Math.Floor(_numTextures.floatValue)));
+                            _numTextures.floatValue = Mathf.Clamp(Mathf.Floor(_numTextures.floatValue), 1.0f, (float)texCount);
+                        }
+
+                        using (new EditorGUI.DisabledScope(true))
+                        {
+                            me.ShaderProperty(_mainTexes[0], GetLoc("sMainTex1"));
+                        }
+                        for (int i = 1; i < _mainTexes.Length; i++)
+                        {
+                            EditorGUILayout.Space();
+                            using (new EditorGUI.DisabledScope(i > texCount))
+                            {
+                                me.ShaderProperty(_mainTexes[i], GetLoc("sMainTex" + (i + 1)));
+                            }
                         }
                     }
+                    else if (texMode == TexMode.TextureArray)
+                    {
+                        var texValue = _mainTexArray.textureValue;
+                        var depth = texValue == null ? 0.0f : (float)((Texture2DArray)texValue).depth;
+                        using (new EditorGUI.DisabledScope(texValue is null))
+                        {
+                            me.ShaderProperty(_numTextures, GetLoc("sNumTextures"));
+                            if (GUILayout.Button("Set to texture count"))
+                            {
+                                _numTextures.floatValue = depth;
+                            }
+                            else
+                            {
+                                _numTextures.floatValue = Mathf.Clamp(Mathf.Floor(_numTextures.floatValue), 1.0f, depth);
+                            }
+                        }
+                        me.ShaderProperty(_mainTexArray, GetLoc("sMainTexArray"));
+                    }
+                    else
+                    {
+                        me.ShaderProperty(_numTextures, GetLoc("sNumTextures"));
+                        me.ShaderProperty(_atlasRows, GetLoc("sAtlasRows"));
+                        me.ShaderProperty(_atlasCols, GetLoc("sAtlasCols"));
+                        _numTextures.floatValue = Mathf.Clamp(_numTextures.floatValue, 1.0f, _atlasRows.floatValue * _atlasCols.floatValue);
+                        if (texMode == TexMode.MainTextureAsAtlas)
+                        {
+                            using (new EditorGUI.DisabledScope(true))
+                            {
+                                me.ShaderProperty(_mainTexes[0], GetLoc("sMainTex1"));
+                            }
+                        }
+                        else
+                        {
+                            me.ShaderProperty(_mainTexes[1], GetLoc("sMainTex2"));
+                        }
+                    }
+                }
+            }
 
-                    me.ShaderProperty(_displayCycleTime, GetLoc("sDisplayTime"));
-                    me.ShaderProperty(_crossFadeTime, GetLoc("sCrossFadeTime"));
+            foreach (var keyword in material.shaderKeywords)
+            {
+                if (!IsMultiKeyword(keyword))
+                {
+                    _shaderKeywords.Add(keyword);
                 }
             }
         }
@@ -174,6 +286,51 @@ namespace Koturn.lilToon
             ltsmref     = Shader.Find("Hidden/" + ShaderName + "/MultiRefraction");
             ltsmfur     = Shader.Find("Hidden/" + ShaderName + "/MultiFur");
             ltsmgem     = Shader.Find("Hidden/" + ShaderName + "/MultiGem");
+        }
+
+        /// <summary>
+        /// Check keyword is used in lilToonMulti or not.
+        /// </summary>
+        /// <param name="keyword">Shader keyword.</param>
+        /// <returns>True if the keyword is used in lilToonMulti, otherwise false.</returns>
+        private static bool IsMultiKeyword(string keyword)
+        {
+            switch (keyword)
+            {
+                case "ANTI_FLICKER":
+                case "EFFECT_BUMP":
+                case "EFFECT_HUE_VARIATION":
+                case "ETC1_EXTERNAL_ALPHA":
+                case "GEOM_TYPE_BRANCH":
+                case "GEOM_TYPE_BRANCH_DETAIL":
+                case "GEOM_TYPE_FROND":
+                case "GEOM_TYPE_LEAF":
+                case "GEOM_TYPE_MESH":
+                case "PIXELSNAP_ON":
+                case "UNITY_UI_ALPHACLIP":
+                case "UNITY_UI_CLIP_RECT":
+                case "_COLORADDSUBDIFF_ON":
+                case "_COLORCOLOR_ON":
+                case "_COLOROVERLAY_ON":
+                case "_DETAIL_MULX2":
+                case "_EMISSION":
+                case "_FADING_ON":
+                case "_GLOSSYREFLECTIONS_OFF":
+                case "_MAPPING_6_FRAMES_LAYOUT":
+                case "_METALLICGLOSSMAP":
+                case "_NORMALMAP":
+                case "_PARALLAXMAP":
+                case "_REQUIRE_UV2":
+                case "_SMOOTHNESS_TEXTURE_ALBEDO_CHANNEL_A":
+                case "_SPECGLOSSMAP":
+                case "_SPECULARHIGHLIGHTS_OFF":
+                case "_SUNDISK_HIGH_QUALITY":
+                case "_SUNDISK_NONE":
+                case "_SUNDISK_SIMPLE":
+                    return true;
+                default:
+                    return false;
+            }
         }
 
         /// <summary>
--- Assets/koturn/LilCrossFade/Shaders/custom_insert.hlsl
+++ Assets/koturn/LilCrossFade/Shaders/custom_insert.hlsl
@@ -1,3 +1,13 @@
+#if !defined(LIL_OUTLINE)
+#    if defined(_TEXMODE_TEXTURE_ARRAY)
+#        define sampler_MainTex sampler_MainTexArray
+#    elif defined(_TEXMODE_SECOND_TEXTURE_AS_ATLAS)
+SAMPLER(sampler_MainTex2);
+#        define sampler_MainTex sampler_MainTex2
+#    endif  // defined(_TEXMODE_TEXTURE_ARRAY)
+#endif  // !defined(LIL_OUTLINE)
+
+
 /*!
  * @brief Sample _MainTexArray with cross fading.
  * @param [in] fd  Fragment data.
@@ -11,17 +21,42 @@ float4 crossFadeSample(lilFragData fd)
     const float t1 = fmodglsl(LIL_TIME, oneCycleTime * _NumTextures);
     const float texIdx1 = floor(t1 / oneCycleTime);
     const float texIdx2 = fmodglsl(texIdx1 + 1.0, _NumTextures);
-#if !defined(LIL_PASS_FORWARD_NORMAL_INCLUDED) || !defined(LIL_FEATURE_PARALLAX) || !defined(LIL_FEATURE_POM)
+#if defined(_TEXMODE_TEXTURE_ARRAY)
+#    if !defined(LIL_PASS_FORWARD_NORMAL_INCLUDED) || !defined(LIL_FEATURE_PARALLAX) || !defined(LIL_FEATURE_POM)
     const float4 col1 = LIL_SAMPLE_2D_ARRAY(_MainTexArray, sampler_MainTex, fd.uvMain, texIdx1);
     const float4 col2 = LIL_SAMPLE_2D_ARRAY(_MainTexArray, sampler_MainTex, fd.uvMain, texIdx2);
-#elif defined(SHADER_API_D3D9) || (defined(SHADER_TARGET_SURFACE_ANALYSIS) && defined(SHADER_TARGET_SURFACE_ANALYSIS_MOJOSHADER)) || defined(SHADER_TARGET_SURFACE_ANALYSIS)
+#    elif defined(SHADER_API_D3D9) || (defined(SHADER_TARGET_SURFACE_ANALYSIS) && defined(SHADER_TARGET_SURFACE_ANALYSIS_MOJOSHADER)) || defined(SHADER_TARGET_SURFACE_ANALYSIS)
     const float4 col1 = tex2DArraygrad(_MainTexArray, float3(fd.uvMain, texIdx1), fd.uvMain, texIdx1);
     const float4 col2 = tex2DArraygrad(_MainTexArray, float3(fd.uvMain, texIdx2), fd.uvMain, texIdx2);
-#else
+#    else
     const float4 col1 = _MainTexArray.SampleGrad(sampler_MainTex, float3(fd.uvMain, texIdx1), fd.ddxMain, fd.ddyMain);
     const float4 col2 = _MainTexArray.SampleGrad(sampler_MainTex, float3(fd.uvMain, texIdx2), fd.ddxMain, fd.ddyMain);
-#endif
-
+#    endif  //  !defined(LIL_PASS_FORWARD_NORMAL_INCLUDED) || !defined(LIL_FEATURE_PARALLAX) || !defined(LIL_FEATURE_POM)
+#elif defined(_TEXMODE_SECOND_TEXTURE_AS_ATLAS)
+    const float2 atlasUv1 = calcAtlasUv(fd.uv0, texIdx1, _AtlasRows, _AtlasCols);
+    const float4 col1 = LIL_SAMPLE_2D(_MainTex2, sampler_MainTex, atlasUv1);
+    const float2 atlasUv2 = calcAtlasUv(fd.uv0, texIdx2, _AtlasRows, _AtlasCols);
+    const float4 col2 = LIL_SAMPLE_2D(_MainTex2, sampler_MainTex, atlasUv2);
+#elif defined(_TEXMODE_MAIN_TEXTURE_AS_ATLAS)
+    const float2 atlasUv1 = calcAtlasUv(fd.uv0, texIdx1, _AtlasRows, _AtlasCols);
+    const float4 col1 = LIL_SAMPLE_2D(_MainTex, sampler_MainTex, atlasUv1);
+    const float2 atlasUv2 = calcAtlasUv(fd.uv0, texIdx2, _AtlasRows, _AtlasCols);
+    const float4 col2 = LIL_SAMPLE_2D(_MainTex, sampler_MainTex, atlasUv2);
+#else
+    float4 cols[4];
+    cols[0] = LIL_SAMPLE_2D(_MainTex, sampler_MainTex, fd.uvMain);
+    if (_NumTextures > 1) {
+        cols[1] = LIL_SAMPLE_2D(_MainTex2, sampler_MainTex, fd.uvMain);
+    }
+    if (_NumTextures > 2) {
+        cols[2] = LIL_SAMPLE_2D(_MainTex3, sampler_MainTex, fd.uvMain);
+    }
+    if (_NumTextures > 3) {
+        cols[3] = LIL_SAMPLE_2D(_MainTex4, sampler_MainTex, fd.uvMain);
+    }
+    const float4 col1 = cols[texIdx1];
+    const float4 col2 = cols[texIdx2];
+#endif  // defined(_TEXMODE_TEXTURE_ARRAY)
     const float t2 = fmodglsl(_Time.y, oneCycleTime);
     const float blendCoeff = saturate(t2 / crossFadeTime);
 
