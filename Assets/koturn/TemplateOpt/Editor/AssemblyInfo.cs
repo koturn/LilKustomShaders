1ff095a4795d4a66f9b7463382145f81954395c6
koturn <jeak.koutan.apple@gmail.com>
2024-12-22 12:03:44 +0900

    chore: Add comments to AssemblyInfo.cs

--- Assets/koturn/TemplateOpt/Editor/AssemblyInfo.cs
+++ Assets/koturn/TemplateOpt/Editor/AssemblyInfo.cs
@@ -1,12 +1,36 @@
 using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyCompany("koturn")]
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCopyright("Copyright (C) 2022 koturn All Rights Reserverd.")]
-[assembly: AssemblyDescription("Material inspector of koturn/TemplateOpt.")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
-[assembly: AssemblyInformationalVersion("1.1.2.0")]
-[assembly: AssemblyProduct("Koturn.TemplateOpt")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Koturn.TemplateOpt")]
-[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/TemplateOpt")]
+[assembly: AssemblyDescription("Material inspector of koturn/TemplateOpt.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("koturn")]
+[assembly: AssemblyProduct("Koturn.TemplateOpt")]
+[assembly: AssemblyCopyright("Copyright (C) 2023 koturn All Rights Reserverd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("15de12e2-3c65-fb94-0b04-e3ed49db87b3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
+[assembly: AssemblyInformationalVersion("1.1.4.0")]
+
+// Metadata
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/LillKustomShaders")]
