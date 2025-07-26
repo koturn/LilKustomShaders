e225b9a30cc1b3cfb43e01e86d2173583a1daa36
koturn <jeak.koutan.apple@gmail.com>
2025-07-26 13:46:11 +0900

    refactor: Resolve a warning, UNT0031

--- Assets/koturn/LilWireframe/Editor/Startup.cs
+++ Assets/koturn/LilWireframe/Editor/Startup.cs
@@ -8,17 +8,35 @@ namespace Koturn.lilToon
     /// <summary>
     /// Startup method provider.
     /// </summary>
-    internal static class Startup
+    internal sealed class Startup : AssetPostprocessor
     {
+#if UNITY_2021_2_OR_NEWER
         /// <summary>
-        /// A method called at Unity startup.
+        /// This is called after importing of any number of assets is complete.
         /// </summary>
-        [InitializeOnLoadMethod]
-#pragma warning disable IDE0051 // Remove unused private members
-        private static void OnStartup()
-#pragma warning restore IDE0051 // Remove unused private members
+        /// <param name="importedAssets">Array of paths to imported assets.</param>
+        /// <param name="deletedAssets">Array of paths to deleted assets.</param>
+        /// <param name="movedAssets">Array of paths to moved assets.</param>
+        /// <param name="movedFromAssetPaths">Array of original paths for moved assets.</param>
+        /// <param name="didDomainReload">Boolean set to true if there has been a domain reload.</param>
+        /// <remarks>
+        /// <seealso href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/AssetPostprocessor.OnPostprocessAllAssets.html"/>
+        /// </remarks>
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
+#else
+        /// <summary>
+        /// This is called after importing of any number of assets is complete.
+        /// </summary>
+        /// <param name="importedAssets">Array of paths to imported assets.</param>
+        /// <param name="deletedAssets">Array of paths to deleted assets.</param>
+        /// <param name="movedAssets">Array of paths to moved assets.</param>
+        /// <param name="movedFromAssetPaths">Array of original paths for moved assets.</param>
+        /// <remarks>
+        /// <seealso href="https://docs.unity3d.com/2019.4/Documentation/ScriptReference/AssetPostprocessor.OnPostprocessAllAssets.html"/>
+        /// </remarks>
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+#endif  // UNITY_2021_2_OR_NEWER
         {
-            AssetDatabase.importPackageCompleted += Startup_ImportPackageCompleted;
             UpdateIncludeFiles();
         }
 
@@ -78,19 +96,6 @@ namespace Koturn.lilToon
             {
                 Debug.LogFormat("Update {0}", dstFilePath);
             }
-        }
-
-        /// <summary>
-        /// A callback method for <see cref="AssetDatabase.importPackageCompleted"/>.
-        /// </summary>
-        /// <param name="packageName">Imported package name.</param>
-        private static void Startup_ImportPackageCompleted(string packageName)
-        {
-            if (packageName != "LilOptimized" && !packageName.StartsWith("lilToon"))
-            {
-                return;
-            }
-            UpdateIncludeFiles();
         }
     }
 }
