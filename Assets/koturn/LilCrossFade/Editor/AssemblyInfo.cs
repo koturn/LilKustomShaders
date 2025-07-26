8ad5f4bed468f72f8e82e94e0a2a53031f804f13
koturn <jeak.koutan.apple@gmail.com>
2025-07-26 18:07:21 +0900

    chore: Bump up version number: 1.1.4 -> 1.2.0

--- Assets/koturn/LilCrossFade/Editor/AssemblyInfo.cs
+++ Assets/koturn/LilCrossFade/Editor/AssemblyInfo.cs
@@ -28,9 +28,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.4.0")]
-[assembly: AssemblyFileVersion("1.1.4.0")]
-[assembly: AssemblyInformationalVersion("1.1.4.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0.0")]
 
 // Metadata
 [assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/LillKustomShaders")]
